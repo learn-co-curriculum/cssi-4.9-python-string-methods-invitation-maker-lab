b0f8ab7e45916c0e3e280aa92598bc808c0bd2d3
georgiadavis <gyang925@gmail.com>
2015-07-16 10:38:22 -0700

    remove solution

--- python-invitation-maker-lab.py
+++ python-invitation-maker-lab.py
@@ -7,7 +7,7 @@
 
 percy_invitation = "The family of Percy Weasley proudly invite you to their graduation commencement on Saturday the 22nd of May 1993. Festivities will be held at The Burrow. See you then!"
 
-ron_invitation= percy_invitation.replace("Saturday the 22nd of May 1993","Sunday the 18th of May 1997")
+ron_invitation=
 
 print ron_invitation
 
@@ -32,11 +32,11 @@ print ron_invitation
 #As in Challenge 1, you'll want to use puts to print out your
 #solution to the screen.
 
-name= "Ginny"
-date="17th of May"
-year="1998"
-day= "Sunday"
+name=
+date=
+year=
+day=
 
-ginny_invitation = "The family of %s Weasley proudly invite you to their graduation commencement on %s %s %s. Festivities will be held at The Burrow. See you then!" % (name, day, date, year)
+ginny_invitation =
 
 print ginny_invitation
